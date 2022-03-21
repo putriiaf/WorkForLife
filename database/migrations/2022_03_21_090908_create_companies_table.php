d5416415869ca32cf372d6ae5b42ca68a4868b56
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-03-21 18:19:14 +0700

    push project + migrations

---
+++ database/migrations/2022_03_21_090908_create_companies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('email')->unique();
+            $table->string('namaCP');
+            $table->string('noCP');
+            $table->string('alamat');
+            $table->string('nama_perusahaan');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
---
+++ database/migrations/2022_03_21_091124_create_vacancies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVacanciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vacancies', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('company_id')->references('id')->on('companies');
+            $table->string('posisi');
+            $table->string('jobdesc');
+            $table->string('kriteria');
+            $table->string('domisili');
+            $table->string('link_pendaftaran');
+            //$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vacancies');
+    }
+}
