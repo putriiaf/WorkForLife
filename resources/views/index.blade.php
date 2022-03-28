c20ca9de0755e998fd33ab650eeabc25354526bb
hbdoca <hbdocasyg@gmail.com>
2022-03-28 19:02:10 +0700

    landingpage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <title>WorkForLife</title>
+</head>
+<body>
+<!--  Hero -->
+<section
+    class="pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-36 md:px-12 px-4">
+    <div class="md:flex-1 md:mr-10">
+      <h1 class="font-montserrat text-4xl font-bold mb-7">
+        Mulai Karirmu Disini
+      </h1>
+      <p class="font-montserrat font-light mb-7">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum harum
+        tempore consectetur voluptas, cumque nobis laboriosam voluptatem.
+      </p>
+      <div class="font-montserrat">
+        <input type="search" placeholder="Search" class="bg-white px-6 py-4 rounded-lg border-2 border-black border-solid mr-2 mb-2">
+        </input>
+      </div>
+    </div>
+    <div class="flex justify-around md:block mt-8 md:mt-0 md:flex-1">
+      <img src='#' alt="Gambar" />
+    </div>
+  </section>
+
+  <!-- Update -->
+  <section class="sectionSize mb-72 py-0">
+    <div>
+      <h2 class="text-center text-5xl mb-0">Loker Baru</h2>
+    </div>
+    <div class="flex w-full flex-col md:flex-row">
+
+      <div class='flex-1 flex flex-col mx-6 shadow-2xl relative bg-white rounded-2xl py-5 px-8 my-8 md:top-24'>
+        <div>
+            <img src="#" alt="logo-gojek"> 
+        </div>
+        <h3 class="font-pt-serif font-normal text-2xl text-center mb-4">
+          Gojek
+        </h3>
+        <div class="font-montserrat font-bold text-2xl text-center mb-4">
+          $25
+          <span class="font-normal text-base"> / month</span>
+        </div>
+        <button class=" border-2 border-solid border-black rounded-xl text-lg py-3 mt-4">
+          Apply
+        </button>
+      </div>
+
+      <div class='flex-1 flex flex-col mx-6 shadow-2xl relative bg-white rounded-2xl py-5 px-8 my-8 md:top-24'>
+        <div>
+            <img src="#" alt="logo-gojek"> 
+        </div>
+        <h3 class="font-pt-serif font-normal text-2xl text-center mb-4">
+          Gojek
+        </h3>
+        <div class="font-montserrat font-bold text-2xl text-center mb-4">
+          $25
+          <span class="font-normal text-base"> / month</span>
+        </div>
+        <button class=" border-2 border-solid border-black rounded-xl text-lg py-3 mt-4">
+          Apply
+        </button>
+      </div>
+
+      <div class='flex-1 flex flex-col mx-6 shadow-2xl relative bg-white rounded-2xl py-5 px-8 my-8 md:top-24'>
+        <div>
+            <img src="#" alt="logo-gojek"> 
+        </div>
+        <h3 class="font-pt-serif font-normal text-2xl text-center mb-4">
+          Gojek
+        </h3>
+        <div class="font-montserrat font-bold text-2xl text-center mb-4">
+          $25
+          <span class="font-normal text-base"> / month</span>
+        </div>
+        <button class=" border-2 border-solid border-black rounded-xl text-lg py-3 mt-4">
+          Apply
+        </button>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Features -->
+  <section class="sectionSize bg-secondary">
+    <div>
+      <h2 class="secondaryTitle bg-underline3 bg-100% text-center text-5xl">Features</h2>
+    </div>
+    <div class="md:grid md:grid-cols-2 md:grid-rows-2 mt-10">
+
+      <div class="flex items-start font-montserrat my-6 mr-10">
+        <img src='dist/assets/logos/Heart.svg' alt='' class="h-7 mr-4" />
+        <div>
+          <h3 class="font-semibold text-2xl">Feature #1</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quisquam voluptate praesentium tenetur earum repellendus! Dicta
+            culpa consequuntur saepe quibusdam labore, est ex ducimus
+            tempore, quos illum officiis, pariatur ea placeat.
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start font-montserrat my-6 mr-10">
+        <img src='dist/assets/logos/Heart.svg' alt='' class="h-7 mr-4" />
+        <div>
+          <h3 class="font-semibold text-2xl">Feature #2</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quisquam voluptate praesentium tenetur earum repellendus! Dicta
+            culpa consequuntur saepe quibusdam labore, est ex ducimus
+            tempore, quos illum officiis, pariatur ea placeat.
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start font-montserrat my-6 mr-10">
+        <img src='dist/assets/logos/Heart.svg' alt='' class="h-7 mr-4" />
+        <div>
+          <h3 class="font-semibold text-2xl">Feature #3</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quisquam voluptate praesentium tenetur earum repellendus! Dicta
+            culpa consequuntur saepe quibusdam labore, est ex ducimus
+            tempore, quos illum officiis, pariatur ea placeat.
+          </p>
+        </div>
+      </div>
+
+      <div class="flex items-start font-montserrat my-6 mr-10">
+        <img src='dist/assets/logos/Heart.svg' alt='' class="h-7 mr-4" />
+        <div>
+          <h3 class="font-semibold text-2xl">Feature #4</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Quisquam voluptate praesentium tenetur earum repellendus! Dicta
+            culpa consequuntur saepe quibusdam labore, est ex ducimus
+            tempore, quos illum officiis, pariatur ea placeat.
+          </p>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- Testi -->
+  <section>
+    <div>
+        <h2 class="secondaryTitle bg-underline3 bg-100% text-center text-5xl mt-10">
+            Testi
+        </h2>
+    </div>
+  </section>
+
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: 
+    {
+      fontFamily:{
+        'montserrat': ['Montserrat']
+      },
+      textColor:{
+        'pingki':'#E84A5F'
+      }
+    },
+  },
+  plugins: [],
+}
