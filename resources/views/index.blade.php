3a3b0fb4617798f318a4a7bbb3fc5b17cc7f8e92
Muhammad Fadlan <fasyafadlan7@gmail.com>
2022-04-09 15:10:19 +0700

    navbar + landing page again

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,18 +10,22 @@
     <title>WorkForLife</title>
 </head>
 <body>
+-->
+
+@extends('layout.layout')
+
 <!--  Hero -->
-<section class="pt-24 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-20 md:px-12 px-4">
+@section('content')
+<section class="relative pt-10 md:mt-0 md:h-screen flex flex-col justify-center text-center md:text-left md:flex-row md:justify-between md:items-center lg:px-20 md:px-12 px-4">
     <div class="md:flex-1 md:mr-40">
-      <h1 class="font-montserrat text-5xl font-bold mb-7">Mulai <span class="text-pingki">Karirmu</span> Disini</h1>
+      <h1 class="font-montserrat text-5xl font-extrabold mb-7">Mulai <span class="text-pingki">Karirmu</span> Disini</h1>
       <p class="font-montserrat font-light mb-7 max-w-xl">
       Dapatkan pekerjaan yang cocok untukmu, cari tahu lebih lewat pengalaman orang lain, dan apply lewat WorkForLife!
       </p>
       <div>
   	    <form class="font-montserrat flex">
-    	    <input class="rounded-l-lg p-4 pr-44 border-t mr-0 border-b border-l border-gray-200 bg-white shadow-xl" placeholder="Temukan pekerjaan atau perusahaan.."/>
-		      <button class="px-8 rounded-r-lg bg-[#E84A5F]  text-white font-bold p-4  border-[#E84A5F] hover:bg-[#E84A5F]/75 border-[#E84A5F]/75">Cari</button>
-	      </form>
+		    <button class="px-8 rounded-lg bg-[#E84A5F]  text-white font-bold p-4  border-[#E84A5F] hover:bg-[#E84A5F]/75 border-[#E84A5F]/75">Cek Sekarang</button>
+	    </form>
       </div>
     </div>
     <div class="flex justify-around md:block mt-8 md:mt-0 md:flex-1">
@@ -28,14 +33,53 @@
     </div>
   </section>
 
-<section class = "md:mt-0 flex flex-col justify-center lg-px-20 md:px-12 px-4 bg-teal-200">
-    Mitra Kami
-</section>
-
+  <section class="font-montserrat">
+  <div class="py-8 top-8"></div>
+  <div class="sm:max-w-3xl pt-8 rounded-xl bg-[#F2F6FB] mx-4 sm:mx-8 md:mx-auto">
+    <div class="w-11/12 sm:w-2/3 mx-auto mb-6">
+      <h1 class="focus:outline-none xl:text-xl text-lg text-center text-gray-800 font-extrabold pt-2">Perusahaan Terpercaya</h1>
+    </div>
+    <div class="sm:py-6 px-8 sm:24 flex flex-wrap items-center">
+      <div
+        class="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+        <img class="focus:outline-none w-12 sm:w-16 " src="https://seeklogo.com/images/G/gojek-logo-D6E639C020-seeklogo.com.png"
+          alt="Gojek" role="img" />
+      </div>
+      <div
+        class="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl ">
+        <img class="focus:outline-none w-12 sm:w-20" src="{{asset('img/traveloka.png')}}"
+          alt="Traveloka" role="img" />
+      </div>
+      <div
+        class="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+        <img class="focus:outline-none w-12 sm:w-16" src="https://seeklogo.com/images/B/blibli-com-logo-CEA6EEEC52-seeklogo.com.png"
+          alt="Nike" role="img" />
+      </div>
+      <div
+        class="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+        <img class="focus:outline-none w-12 sm:w-16" src="https://seeklogo.com/images/G/goto-logo-E7E408295D-seeklogo.com.png"
+          alt="Toyota" role="img" />
+      </div>
+      <div
+        class="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+        <img class="focus:outline-none w-12 sm:w-16" src="https://seeklogo.com/images/T/tokopedia-logo-40654CCDD6-seeklogo.com.png"
+          alt="Toyota" role="img" />
+      </div>
+      <div
+        class="w-1/3 sm:w-1/6 flex justify-center xl:pb-10 pb-16 items-center inset-0 transform  hover:scale-75 transition duration-300 contrast-75 hover:contrast-100 drop-shadow-xl">
+        <img class="focus:outline-none w-12 sm:w-16" src="https://seeklogo.com/images/H/halodoc-logo-5A7C52DE38-seeklogo.com.png"
+          alt="Toyota" role="img" />
+      </div>
+    </div>
+  </div>
+  </section>
 
   <!-- Loker Terbaru -->
-<section>
-  <div class="flex items-center justify-center h-screen bg-[#123C69]">
+<section class="relative bg-[#123C69]">
+    <div class="pt-28 max-h-7">
+        <p class="font-montserrat font-bold pt-5 text-white text-center text-5xl sm:text-4xl">Beberapa Lowongan Terbaru</p>
+    </div>
+  <div class="flex items-center justify-center h-screen">
    <div class="font-montserrat bg-white font-semibold text-center rounded-xl border shadow-lg px-10 py-5 max-w-xs md:m-10">
      <img class="mb-3 w-32 h-32 rounded-lg mx-auto" src="{{ asset('img/gojek.png') }}" alt="logo">
      <h2 class="text-md"> Product Designer </h1>
@@ -63,84 +107,75 @@
 
 <!-- Features -->
 <section class="bg-white mt-10">
-            <div class="container font-montserrat px-6 py-10 mx-auto">
-                <h1 class="text-3xl text-center font-semibold capitalize lg:text-4xl">Kenapa WorkForLife ?</h1>
+  <div class="container font-montserrat px-6 py-10 mx-auto">
+    <h1 class="text-3xl text-center font-bold capitalize lg:text-4xl">Kenapa Harus WorkForLife?</h1>
 
-                <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2">
-                    <div class="p-6 border rounded-xl border-r-gray-200">
-                        <div class="md:flex md:items-start md:-mx-4">
-                            <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" />
-                                </svg>
-                            </span>
+    <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2">
+      <div class="p-6">
+        <div class="md:flex md:items-start md:-mx-4">
+          <span class="inline-block px-2 py-5 rounded-xl md:mx-4">
+            <img class="w-32 h-20" src="{{ asset('img/trust.png') }}" alt="">
+          </span>
 
-                            <div class="mt-4 md:mx-4 md:mt-0">
-                                <h1 class="text-2xl font-medium text-gray-700 capitaliz">Copy & paste components</h1>
+          <div class="pt-3 mt-4 md:mx-4 md:mt-0">
+            <h1 class="text-2xl font-bold text-pingki capitaliz">AMAN DAN TERPERCAYA</h1>
 
-                                <p class="mt-3 text-gray-500">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+            <p class="mt-3">
+              Perusahaan di Work For Life sudah melewati proses verifikasi dan dijamin kredibilitasnya
+            </p>
+          </div>
+        </div>
+      </div>
 
-                    <div class="p-6 border rounded-xl border-r-gray-200 dark:border-gray-700">
-                        <div class="md:flex md:items-start md:-mx-4">
-                            <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
-                                </svg>
-                            </span>
+      <div class="p-6">
+        <div class="md:flex md:items-start md:-mx-4">
+          <span class="inline-block px-2 py-5 rounded-xl md:mx-4">
+            <img class="w-32 h-20" src="{{ asset('img/loker.png') }}" alt="">
+          </span>
 
-                            <div class="mt-4 md:mx-4 md:mt-0">
-                                <h1 class="text-2xl font-medium text-gray-700 capitali">Zero Configrations</h1>
+          <div class="pt-3 mt-4 md:mx-4 md:mt-0">
+            <h1 class="text-2xl font-bold text-pingki capitaliz">LOKER BERVARIASI</h1>
 
-                                <p class="mt-3 text-gray-500 dark:text-gray-300">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+            <p class="mt-3">
+              Jenis lowongan bervariasi berdasarkan bidang, lokasi, dan kualifikasi
+            </p>
+          </div>
+        </div>
+      </div>
 
-                    <div class="p-6 border rounded-xl border-r-gray-200 dark:border-gray-700">
-                        <div class="md:flex md:items-start md:-mx-4">
-                            <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
-                                </svg>
-                            </span>
+      <div class="p-6">
+        <div class="md:flex md:items-start md:-mx-4">
+          <span class="inline-block px-2 py-5 rounded-xl md:mx-4">
+            <img class="w-32 h-20" src="{{ asset('img/update.png') }}" alt="">
+          </span>
 
-                            <div class="mt-4 md:mx-4 md:mt-0">
-                                <h1 class="text-2xl font-medium text-gray-700 capitaliz">elegant Dark Mode</h1>
+          <div class="pt-3 mt-4 md:mx-4 md:mt-0">
+            <h1 class="text-2xl font-bold text-pingki capitaliz">SELALU UP TO DATE</h1>
 
-                                <p class="mt-3 text-gray-500 dark:text-gray-300">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+            <p class="mt-3">
+              Lowongan kerja selalu diurutkan dari yang paling terbaru dan diupdate
+            </p>
+          </div>
+        </div>
+      </div>
 
-                    <div class="p-6 border rounded-xl border-r-gray-200 dark:border-gray-700">
-                        <div class="md:flex md:items-start md:-mx-4">
-                            <span class="inline-block p-2 text-blue-500 bg-blue-100 rounded-xl md:mx-4 dark:text-white dark:bg-blue-500">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
-                                </svg>
-                            </span>
+      <div class="p-6">
+        <div class="md:flex md:items-start md:-mx-4">
+          <span class="inline-block px-2 py-5 rounded-xl md:mx-4">
+            <img class="w-32 h-20" src="{{ asset('img/sharing.png') }}" alt="">
+          </span>
 
-                            <div class="mt-4 md:mx-4 md:mt-0">
-                                <h1 class="text-2xl font-medium text-gray-700 capitaliz">Simple & clean designs</h1>
+        <div class="pt-3 mt-4 md:mx-4 md:mt-0">
+          <h1 class="text-2xl font-bold text-pingki capitaliz">SHARING IS HELPING</h1>
 
-                                <p class="mt-3 text-gray-500 dark:text-gray-300">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+          <p class="mt-3">
+            Fitur forum membantu kamu untuk memahami karir dan menambah wawasan
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
 </section>
 
 <!-- Testimoni -->
@@ -151,7 +186,9 @@
     </h2>
   </div>
 </section>
+@endsection
 
-
+<!--
 </body>
 </html>
+-->
