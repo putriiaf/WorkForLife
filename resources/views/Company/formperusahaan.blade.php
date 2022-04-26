87013418d53d050fb40f794381c650962ac204ac
Muhammad Fadlan <fasyafadlan7@gmail.com>
2022-04-26 14:17:04 +0700

    update form perusahaan

--- resources/views/Company/formperusahaan.blade.php
+++ resources/views/Company/formperusahaan.blade.php
@@ -7,18 +7,34 @@
     </div>
 
     @if(session()->has('success'))
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        {{ session('success') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    @endif
+      <div id="alert-3" class="flex p-4 mb-4 bg-green-100 rounded-lg " role="alert">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+          </svg>
+          <div class="ml-3 text-sm font-medium text-green-700 dark:text-green-800">
+          {{ session('success') }}
+          </div>
+          <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-green-100 text-green-500 rounded-lg focus:ring-2 focus:ring-green-400 p-1.5 hover:bg-green-200 inline-flex h-8 w-8 dark:bg-green-200 dark:text-green-600 dark:hover:bg-green-300" data-dismiss-target="#alert-3" aria-label="Close">
+            <span class="sr-only">Close</span>
+            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+          </button>
+      </div>
+      @endif
 
-    @if(session()->has('loginError'))
-    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-        {{ session('loginError') }}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    @endif
+      @if(session()->has('loginError'))
+      <div id="alert-2" class="flex p-4 mb-4 bg-red-100 rounded-lg mx-20 max-w-[400px]" role="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-700" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+        <div class="ml-3 text-sm font-medium text-red-700">
+          {{ session('loginError') }}
+        </div>
+        <button type="button" class="ml-auto -mx-1.5 -my-1.5 bg-red-100 text-red-500 rounded-lg focus:ring-2 focus:ring-red-400 p-1.5 hover:bg-red-200 inline-flex h-8 w-8 dark:bg-red-200 dark:text-red-600 dark:hover:bg-red-300" data-dismiss-target="#alert-2" aria-label="Close">
+          <span class="sr-only">Close</span>
+          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
+      </div>
+      @endif
 
     <div class="flex justify-center item-center">
         <form method="POST" action="/company/verify" class="w-6/12" enctype="multipart/form-data">
@@ -29,7 +45,7 @@
                 <div class="w-44 font-bold h-6 mx-2 mt-3">Nama Perusahaan</div>
                 <div class="flex-1 flex flex-col md:flex-row">
                     <div class="w-full flex-1 mx-2">
-                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded-lg">
                             <input type="text" class="p-1 px-2 w-full @error('nama_perusahaan') is-invalid @enderror" name="nama_perusahaan" value="{{ old('nama_perusahaan') }}">
                             @error('nama_perusahaan')
                             <div class="invalid-feedback">
@@ -44,7 +60,7 @@
                 <div class="w-44 font-bold h-6 mx-2 mt-3">Alamat</div>
                 <div class="flex-1 flex flex-col md:flex-row">
                     <div class="w-full flex-1 mx-2">
-                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded-lg">
                             <input type="text" class="p-1 px-2 py-10 w-full @error('alamat') is-invalid @enderror" name="alamat" value="{{ old('alamat') }}">
                             @error('alamat')
                             <div class="invalid-feedback">
@@ -59,7 +75,7 @@
                 <div class="w-44 font-bold h-6 mx-2 mt-3">Nama Contact Perusahaan</div>
                 <div class="flex-1 flex flex-col md:flex-row">
                     <div class="w-full flex-1 mx-2">
-                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded-lg">
                             <input type="text" class="p-1 px-2 w-full @error('namaCP') is-invalid @enderror" name="namaCP" value="{{ old('namaCP') }}">
                             @error('namaCP')
                             <div class="invalid-feedback">
@@ -74,7 +90,7 @@
                 <div class="w-44 font-bold h-6 mx-2 mt-3">Nama Telepon Contact Perusahaan</div>
                 <div class="flex-1 flex flex-col md:flex-row">
                     <div class="w-full flex-1 mx-2">
-                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded-lg">
                             <input type="text" class="p-1 px-2 w-full @error('noCP') is-invalid @enderror" name="noCP" value="{{ old('noCP') }}">
                             @error('noCP')
                             <div class="invalid-feedback">
@@ -89,7 +105,7 @@
                 <div class="w-44 text-base font-bold h-6 mx-2 mt-3">Email</div>
                 <div class="flex-1 flex flex-col md:flex-row">
                     <div class="w-full flex-1 mx-2">
-                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded">
+                        <div class="my-2 p-1 bg-white flex border border-gray-200 rounded-lg">
                             <input type="text" class="p-1 px-2 w-full @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}">
                             @error('email')
                             <div class="invalid-feedback">
