8c4af3ca283dfaed1ee7ae2fb95477f167d6a1f9
Muhammad Fadlan <fasyafadlan7@gmail.com>
2022-04-28 15:19:56 +0700

    update mobile menu

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -65,76 +65,47 @@
                       @endif
                     </a>
                 </div>
-                <h3 class="mx-2 text-sm font-medium text-gray-700 dark:text-gray-200 md:hidden">Khatab wedaa</h3>
             </button>
 		</div>
 		@endguest
+    <div class="md:hidden flex items-center">
+						<button class="outline-none mobile-menu-button">
+						<svg class=" w-6 h-6 text-gray-500 hover:text-dongker "
+							x-show="!showMenu"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button>
+					</div>
+				</div>
+			</div>
+			<!-- mobile menu -->
+			<div class="hidden mobile-menu mx-10">
+				<ul class="">
+					<li><a href="/" class="block text-sm px-2 py-4 bg-white hover:bg-dongker hover:text-white transition duration-300">Beranda</a></li>
+					<li><a href="/loker" class="block text-sm px-2 py-4 bg-white hover:bg-dongker hover:text-white transition duration-300">Cari Loker</a></li>
+					<li><a href="/posts" class="block text-sm px-2 py-4 bg-white hover:bg-dongker hover:text-white transition duration-300">Sharing</a></li>
+					<li><a href="/admin" class="block text-sm px-2 py-4 bg-white hover:bg-dongker hover:text-white transition duration-300">Admin</a></li>
+				</ul>
+			</div>
 	</div>
+
+      <script>
+				const btn = document.querySelector("button.mobile-menu-button");
+				const menu = document.querySelector(".mobile-menu");
+
+				btn.addEventListener("click", () => {
+					menu.classList.toggle("hidden");
+				});
+			</script>
 </nav>
 
-
-<!--
-<nav class="border-gray-200 px-2 sm:px-4 py-6 font-montserrat">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
-  <a href="/" class="flex items-center pl-6">
-      <img src="{{ asset('img/logo.png') }}" class="mr-3 w-26 h-12" alt="Flowbite Logo" />
-  </a>
-  @guest
-  <div class="flex md:order-2 space-x-5">
-    @if (Route::has('login'))
-    <form action="/company">
-      <button type="button" class="flex text-dongker bg-white border-2 border-[#123C69] hover:bg-white/40 hover:border-[#123C69]/40 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 ">Rekrut Sekarang</button>
-    </form>
-    @endif
-    @if (Route::has('login'))
-    <form action="/login">
-      <button href="/login" type="button" class="flex text-white bg-dongker border-2 border-[#123C69] hover:bg-dongker/40 hover:border-[#123C69]/40 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Masuk</button>
-    </form>
-    @endif
-
-    @else
-    <form action="/logout" method="post">
-      @csrf
-      <button type="button" class="text-white bg-dongker border-2 border-[#123C69] hover:bg-dongker/40 hover:border-[#123C69]/40 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0">Keluar</button>
-    </form>
-
-    <button type="button" class="flex mr-3 text-sm rounded-full md:mr-0" id="user-menu-button" aria-expanded="false" type="button" data-dropdown-toggle="dropdown">
-      <a href="/profile">
-      @if(auth()->user()->foto_profil)
-        <img class="w-10 h-10 rounded-full" src="{{ asset('storage/' . auth()->user()->foto_profil) }}" alt="user photo">
-      @else
-      <img class="w-10 h-10 rounded-full" src="{{ asset('img/avatar.png') }}" alt="user photo">
-      @endif
-      </a>
-    </button>
-
-    @endguest
-
-      <button data-collapse-toggle="mobile-menu-4" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-4" aria-expanded="false">
-      <span class="sr-only">Open main menu</span>
-      <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-      <svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-    </button>
-  </div>
-  <div class="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
-  <ul class="flex flex-col mt-4 md:flex-row md:space-x-10 md:mt-0 md:text-md md:font-medium">
-      <li>
-        <a href="/" class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-dongker md:p-0">Beranda</a>
-      </li>
-      <li>
-        <a href="/loker" class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-dongker md:p-0">Cari Loker</a>
-      </li>
-      <li>
-        <a href="/posts" class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-dongker md:p-0">Sharing</a>
-      </li>
-      <li>
-        <a href="/admin" class="block py-2 pr-4 pl-3 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-dongker md:p-0">Admin</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
--->
 
 	<!-- START MAIN -->
   <div class="py-4">
