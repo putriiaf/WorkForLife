f4b5943a61aa005f4d65507d7227a13e1913136b
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-11 22:36:17 +0700

    fix register <solved>

--- resources/views/Register/index.blade.php
+++ resources/views/Register/index.blade.php
@@ -74,6 +74,7 @@
               <input id="jk" type="radio" name="jk" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" required value="P" {{ old('jk') == 'P' ? 'checked' : '' }}>
               <label for="jk" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">P</label>
             </div>
+            <input id="role" type="hidden" name="role" required value="0">
             </div>
           </div>
         </div>
