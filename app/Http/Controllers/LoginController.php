587df2f9fe17576f9ef9b4841ef685185c3d2a9e
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-05-16 21:51:24 +0700

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,7 +9,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        return view('login.index', [
+        return view('Login.index', [
             'title' => 'Login',
             'active' => 'login'
         ]);
