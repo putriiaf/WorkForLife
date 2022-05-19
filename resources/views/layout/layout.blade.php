15f64a76a3192be3cbb8e1012b243c76e2ee1227
Muhammad Fadlan <fasyafadlan7@gmail.com>
2022-05-19 12:46:57 +0700

    Update layout.blade.php

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WorkForLife</title>
-    <link rel="icon" href="{{ asset('img/logo1.png') }}">
+    <link rel="icon" href="{{ asset('https://workforlife.herokuapp.com/img/logo1.png') }}">
     <link href="{{ asset('css/testimonial.css') }}" rel="stylesheet">
     <link href="{{ asset('https://workforlife.herokuapp.com/css/app.css') }}" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
