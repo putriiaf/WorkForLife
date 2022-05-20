fa21e98a52f33f95180e88f2831105a75f5ad5b6
Putri Ainur Fitri <putri.ainur.71@gmail.com>
2022-05-20 13:59:47 +0700

    edit dan hapus untuk perusahaan

--- resources/views/Loker/view.blade.php
+++ resources/views/Loker/view.blade.php
@@ -18,6 +18,16 @@
             <h2 class="pt-1 font-extrabold text-md">{{ $loker->insentif }}</h2>
             <a href="{{ $loker->link_pendaftaran }} "class="bg-dongker px-8 py-2 mt-8 text-white block rounded-xl font-semibold uppercase tracking-wide hover:bg-dongker/70 hover:text-white/50">Apply</a>
         </div>
+        @if(session()->get('token') != NULL)
+        @if(session()->get('role') == 1 && (session()->get('company_id') == $loker->company_id))
+        <a href="/loker/{{ $loker->id }}/edit" class="font-medium text-dongker">Edit</a>
+        <form action="/loker/{{ $loker->id }}" method="post">
+            @method('delete')
+            @csrf
+            <button class="font-medium text-dongker" onclick="return confirm('Apakah Anda yakin ingin menghapus lowongan kerja ini?')">Hapus</button>
+        </form>
+        @endif
+        @endif
     </div>
 </section>
 
