1d03c9f7941c0df2df25051bd9ecfd0d9ae4b4f1
Muhammad Fadlan <55647432+fadlanfasya@users.noreply.github.com>
2022-10-16 20:57:37 +0700

    add levelup page

--- routes/web.php
+++ routes/web.php
@@ -78,3 +78,7 @@ Route::delete('/admin/company/delete/{company:id}', [CompanyController::class, '
 Route::get('/admin/report/{report:id}/detail', [ReportController::class, 'edit']);
 Route::put('/admin/report/{report:id}', [ReportController::class, 'update']);
 Route::delete('/admin/report/{id}', [ReportController::class, 'destroy']);
+
+Route::get('/levelup', function () {
+    return view('levelup.levelup');
+});
