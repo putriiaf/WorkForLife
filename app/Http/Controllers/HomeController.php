a63f2ed2064fee1e6504733d325406976337e928
Alifa Hafida Anwar <alifahafida@gmail.com>
2022-11-07 12:24:56 +0700

    integration BE Home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,18 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Post;
-use App\Models\Vacancy;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $post = Http::get('http://apiwfl.herokuapp.com/api/posts');
+        $loker = Http::get('http://apiwfl.herokuapp.com/api/lokers');
+        $post = $post->object();
+        $loker = $loker->object();
         return view('index', [
             'title' => 'Home',
             'active' => 'home',
-            'latest_posts' => Post::latest()->limit(5)->get(),
-            'latest_loker' => Vacancy::latest()->limit(3)->get()
+            'posts' => $post->data,
+            'latest_loker' => $loker->data
         ]);
     }
 }
